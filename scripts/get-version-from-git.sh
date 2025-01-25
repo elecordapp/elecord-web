76b037f7db0e087abcefb1dc060cfd1392cfd012
hazzuk <git@hazz.uk>
2025-01-25 03:39:35 +0000

    chore(scripts): shorten git version to match github

--- scripts/get-version-from-git.sh
+++ scripts/get-version-from-git.sh
@@ -6,6 +6,6 @@
 set -e
 
 # Since the deps are fetched from git, we can rev-parse
-JSSDK_SHA=$(git -C node_modules/matrix-js-sdk rev-parse --short=12 HEAD)
-VECTOR_SHA=$(git rev-parse --short=12 HEAD) # use the ACTUAL SHA rather than assume develop
+JSSDK_SHA=$(git -C node_modules/matrix-js-sdk rev-parse --short=7 HEAD)
+VECTOR_SHA=$(git rev-parse --short=7 HEAD) # use the ACTUAL SHA rather than assume develop
 echo $VECTOR_SHA-js-$JSSDK_SHA
