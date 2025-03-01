6b43188fa159265606af17005471e449c1d1a25e
hazzuk <git@hazz.uk>
2025-03-01 08:23:25 +0000

    chore(build): add local environment build script
    Helper build script, to quickly build the webapp on Windows.

---
+++ build.ps1
@@ -0,0 +1,21 @@
+# script to build the webapp locally on windows
+
+# check if the 'webapp' folder exists and remove it
+$webappPath = "$(Get-Location)\webapp"
+if (Test-Path $webappPath) {
+    Remove-Item -Recurse -Force $webappPath
+    Write-Host "Deleted existing webapp folder." -ForegroundColor Cyan
+}
+
+# install dependencies using Yarn
+yarn install
+Write-Host "Yarn install completed." -ForegroundColor Cyan
+
+# build the app
+yarn build
+Write-Host "Yarn build completed." -ForegroundColor Cyan
+
+# start a Python HTTP server to serve webapp/index.html
+$pythonCommand = "python -m http.server 8000 --directory webapp"
+Write-Host "Running Python HTTP server..." -ForegroundColor Cyan
+Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "-Command $pythonCommand"
